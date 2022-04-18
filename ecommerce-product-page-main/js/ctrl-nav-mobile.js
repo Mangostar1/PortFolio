e7ef3c5ffbbbec6d0cb8fbe44510d4ece62e434c
Omar Zavala <influencia.x.94@gmail.com>
2022-04-18 18:52:27 -0400

    first commit

---
+++ ecommerce-product-page-main/js/ctrl-nav-mobile.js
@@ -0,0 +1,13 @@
+/*   solo se aplica a vercion mobile  */
+
+let closeNav = document.getElementById('close-nav-id');
+let menu = document.getElementById('nav');
+let openNav = document.getElementById('mobile-menu');
+
+openNav.onclick = () => {
+    menu.style.display = 'block';
+}
+
+closeNav.onclick = () => {
+    menu.style.display = 'none';
+}
