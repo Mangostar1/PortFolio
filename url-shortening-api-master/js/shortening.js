d56f0fb9e57b35a7c507ebb36ea48a3d32cb4a62
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-07-27 17:26:50 -0400

    Se guardan avances

--- url-shortening-api-master/js/shortening.js
+++ url-shortening-api-master/js/shortening.js
@@ -1,8 +1,44 @@
+//to api
 let inputText = document.getElementById("input-text");
-let inputBtn = document.getElementById("input-btn");
+let errorLink = document.getElementById("error-link");
 
+//to display
+
+async function fetchApi() {
+    let url = inputText.value;
+    let response = await fetch("https://api.shrtco.de/v2/shorten?url=" + url);
+    let data = await response.json();
+    console.log(data.result.full_short_link);
+
+    function NewDiv() {
+        let divForm = document.getElementById("div-middle-form");
+        let newDiv = document.createElement("div");
+        newDiv.classList.add('newLinkShorten');
+        newDiv.innerHTML = 
+        `<div class="linkTo">
+            <p>
+                ${url}
+            </p>
+        </div>
+        <div class="linkShort">
+            <p>
+                ${data.result.full_short_link}
+            </p>
+            <button class="btn-copy">Copy</button>
+        </div>`;
+        divForm.appendChild(newDiv);
+    }
+    NewDiv();
+}
+
+
+//event listener
 document.addEventListener('click', (e) => {
     if (e.target.id == "input-btn") {
-        console.log(inputText.value);
+        if (inputText.value == "") {
+            errorLink.style.opacity = "1";
+        } else {
+            fetchApi();
+        }
     }
 });
